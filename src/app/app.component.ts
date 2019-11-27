87e768d2eb405e7cb2264208122bd16edb26292e
LuisMayo <luismayovalbuena@outlook.es>
2019-11-27 22:11:37 +0100

    Added serializing/de-serializing for everything except addlers notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'OxenfreeSaveEditor';
     textFile: string;
+    newFile: string;
     saveFile: Object;
+    ommitedPart: string;
 
 
     onLoad(event: Event) {
         const fr = new FileReader();
         fr.addEventListener('load', () => {
             this.textFile = <string>fr.result;
-            this.processTextFile();
+            this.parseSaveGameIntoObject();
+            this.parseObjectIntoSaveGame();
         });
         fr.readAsText((<HTMLInputElement>event.target).files[0]);
     }
 
-    processTextFile() {
-        this.textFile = this.textFile.substring(0, this.textFile.indexOf(',"GlobalVariables"')) + '}' // We don't know how to process Global variables yet
+
+    private parseSaveGameIntoObject() {
+        this.ommitedPart = this.textFile.substring(this.textFile.indexOf(',"CollectedNotes"'));
+        this.textFile = this.textFile.substring(0, this.textFile.indexOf(',"CollectedNotes"')) + '}'; // We don't know how to process Addler's notes yet
         this.saveFile = JSON.parse(this.textFile);
         this.convertStructureIntoObject(this.saveFile);
-        this.convertObjectIntoStructure(this.saveFile);
     }
 
-    convertStructureIntoObject(obj: Object) {
+    private parseObjectIntoSaveGame() {
+        this.convertObjectIntoStructure(this.saveFile);
+        this.newFile = JSON.stringify(this.saveFile);
+        this.newFile = this.newFile.substring(0, this.newFile.length - 1) + this.ommitedPart;
+    }
+
+    private convertStructureIntoObject(obj: Object) {
         for (const prop of Object.keys(obj)) {
             if (typeof obj[prop] === 'string' && obj[prop].charAt(0) === '{') { // It is an object
                 obj[prop] = JSON.parse(obj[prop]);
@@ -36,12 +46,22 @@ export class AppComponent {
         }
     }
 
-    convertObjectIntoStructure(obj: Object) {
+    private convertObjectIntoStructure(obj: Object) {
         for (const prop of Object.keys(obj)) {
-            if (typeof obj[prop] === 'object' && !Array.isArray(obj[prop])) { // It is an object
+             // It is an object (objects made by numerical ID are no real objects for this task)
+            if (this.checkIfObject(obj[prop])) {
                 this.convertObjectIntoStructure(obj[prop]);
                 obj[prop] = JSON.stringify(obj[prop]);
             }
         }
+    }
+    private checkIfObject(obj: unknown) {
+        if (typeof obj === 'object' && !Array.isArray(obj)) {
+            const keys = Object.keys(obj);
+            if (keys && keys.length > 0) {
+                return !keys[0].match(/\d/g);
+            }
+        }
+        return false;
     }
 }
