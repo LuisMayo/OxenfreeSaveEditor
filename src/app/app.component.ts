944386ed317c5ef85db9544657dd5699eb9c7d55
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2019-12-02 18:21:50 +0000

    Improved keys detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,7 +59,7 @@ export class AppComponent {
         if (typeof obj === 'object' && !Array.isArray(obj)) {
             const keys = Object.keys(obj);
             if (keys && keys.length > 0) {
-                return !keys[0].match(/\d/g);
+                return !/^\d$/g.test(keys[0]);
             }
         }
         return false;
